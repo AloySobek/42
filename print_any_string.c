8028379487a5b9a782222751240f39ebef7359ca
Rustam <super.rustamm@gmail.com>
2019-05-14 23:26:20 +0300

    hello

--- print_any_string.c
+++ print_any_string.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/16 13:25:24 by vrichese          #+#    #+#             */
-/*   Updated: 2019/05/14 23:03:22 by marvin           ###   ########.fr       */
+/*   Updated: 2019/05/14 23:25:52 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,12 @@ int			unistrlen(const wchar_t *s)
 	return (size);
 }
 
-int			print_any_string(wchar_t *s, size_t *flags, int *wid, int *pre)
+void		print_any_string(wchar_t *s, size_t *flags, int *wid, int *pre)
 {
 	char	*h;
-	int		size;
 
 	if (!s)
 		s = L"(null)";
-	size = 0;
 	(*flags * UNI) ? h = (char *)s : 0;
 	*wid -= (((unistrlen(s) > *pre && *flags & POI) ? (*pre) : unistrlen(s)));
 	if (!(*flags & BIA))
@@ -47,5 +45,4 @@ int			print_any_string(wchar_t *s, size_t *flags, int *wid, int *pre)
 			*s++ : *h++)) : (g_buff[g_count++] = (*flags & UNI ? *s++ : *h++));
 	while ((*wid)-- > 0 && ((g_count + 1) == BUFF_SIZE ? eject() : 1))
 		g_buff[g_count++] = (*flags << 56) >> 56;
-	return (size);
 }
